0412f64e0fc6fc4d2d5ce36353aa34bddf103727
Ilya Ivanov <static.ila@gmail.com>
2021-03-30 14:11:21 +0300

    Improve tests

--- src/domain/events.spec.ts
+++ src/domain/events.spec.ts
@@ -5,49 +5,59 @@ type TypeDefinitions = {
   "item-removed": string;
 };
 
-it("subscribing to an event and dispatching it should call subscriber", () => {
-  const events = new EventsHandler<TypeDefinitions>();
-  const fn = jest.fn();
-  events.addEventListener("item-selected", fn);
-  events.dispatchEvent("item-selected", 42);
-  expect(fn).toHaveBeenCalledWith(42);
-});
+describe("subscribing to an event", () => {
+  let events: EventsHandler<TypeDefinitions>;
+  let itemSelectedCallback: (e: number) => void;
+  let itemItemRemovedCallback: (e: string) => void;
+  beforeEach(() => {
+    events = new EventsHandler<TypeDefinitions>();
+    itemSelectedCallback = jest.fn();
+    itemItemRemovedCallback = jest.fn();
+  });
+  it("dispatching item-selected it should call corresponding callback", () => {
+    events.addEventListener("item-selected", itemSelectedCallback);
+    events.dispatchEvent("item-selected", 42);
+    expect(itemSelectedCallback).toHaveBeenCalledWith(42);
+  });
 
-it("subscribing to an event then unsubscribing and dispatching it should call subscriber", () => {
-  const events = new EventsHandler<TypeDefinitions>();
-  const fn = jest.fn();
-  const unsub = events.addEventListener("item-selected", fn);
-  unsub();
-  events.dispatchEvent("item-selected", 42);
-  expect(fn).not.toHaveBeenCalled();
-});
+  it("unsubscribing and then dispatching selected event should not call callback", () => {
+    const unsub = events.addEventListener(
+      "item-selected",
+      itemSelectedCallback
+    );
+    unsub();
+    events.dispatchEvent("item-selected", 42);
+    expect(itemSelectedCallback).not.toHaveBeenCalled();
+  });
 
-it("subscribing to an event then unsubscribing and dispatching it should remove all events listeners", () => {
-  const events = new EventsHandler<TypeDefinitions>();
-  const fn = jest.fn();
-  const unsub = events.addEventListener("item-selected", fn);
-  unsub();
-  expect(JSON.stringify(events.events)).toEqual("{}");
-});
+  it("unsubscribing should remove all events listeners", () => {
+    const unsub = events.addEventListener(
+      "item-selected",
+      itemSelectedCallback
+    );
+    unsub();
+    expect(JSON.stringify(events.events)).toEqual("{}");
+  });
 
-it("subscribing to an event and dispatching compound event should trigger both general and specific events", () => {
-  const events = new EventsHandler<TypeDefinitions>();
-  const fn = jest.fn();
-  const fn1 = jest.fn();
-  events.addEventListener("item-selected", fn);
-  events.addCompoundEventListener("item-selected", "id1", fn1);
-  events.dispatchCompundEvent("item-selected", "id1", 42);
-  expect(fn1).toHaveBeenCalledWith(42);
-  expect(fn).toHaveBeenCalledWith(42);
-});
+  it("subscribing to an event and dispatching compound event should trigger both general and specific events", () => {
+    const itemWithId1SelectedCallback = jest.fn();
+    events.addEventListener("item-selected", itemSelectedCallback);
+    events.addCompoundEventListener(
+      "item-selected",
+      "id1",
+      itemWithId1SelectedCallback
+    );
+    events.dispatchCompundEvent("item-selected", "id1", 42);
+    expect(itemSelectedCallback).toHaveBeenCalledWith(42);
+    expect(itemWithId1SelectedCallback).toHaveBeenCalledWith(42);
+  });
 
-it("subscribing to an event and dispatching compound event should trigger both general and specific events", () => {
-  const events = new EventsHandler<TypeDefinitions>();
-  const selectedCallback = jest.fn();
-  const removedCallback = jest.fn();
-  events.addEventListener("item-selected", selectedCallback);
-  events.addEventListener("item-removed", removedCallback);
-  events.dispatchEvent("item-removed", "42");
-  expect(removedCallback).toHaveBeenCalledWith("42");
-  expect(selectedCallback).not.toHaveBeenCalled();
+  it("subscribing to an event and dispatching compound event should not trigger other events", () => {
+    const removedCallback = jest.fn();
+    events.addEventListener("item-selected", itemSelectedCallback);
+    events.addEventListener("item-removed", removedCallback);
+    events.dispatchEvent("item-removed", "42");
+    expect(removedCallback).toHaveBeenCalledWith("42");
+    expect(itemSelectedCallback).not.toHaveBeenCalled();
+  });
 });
