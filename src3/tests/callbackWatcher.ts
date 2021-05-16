e2a3ccaa59799d300e0f9900dbac167cd7075b17
Ilya Ivanov <static.ila@gmail.com>
2021-05-17 00:24:40 +0300

    Add more details to memory leak diagnostic

--- src3/tests/callbackWatcher.ts
+++ src3/tests/callbackWatcher.ts
@@ -11,6 +11,7 @@ const initShortcuts = () => {
     if (e.altKey && e.code == "KeyV") {
       let modelsCount = 0;
       let callbacksCount = 0;
+      let callbacksCountByEvents: Record<string, number> = {};
 
       const traverseModel = (model: ItemModel) => {
         modelsCount += 1;
@@ -18,12 +19,24 @@ const initShortcuts = () => {
           (count, arr) => count + arr.length,
           0
         );
+        Object.entries(model.events).forEach(([key, value]) => {
+          callbacksCountByEvents[key] =
+            (callbacksCountByEvents[key] || 0) + value.length;
+        });
         model.getChildren().forEach(traverseModel);
       };
       itemModels.forEach(traverseModel);
       console.log(
         `${modelsCount} models and in total ${callbacksCount} callbacks`
       );
+
+      console.log(`Events: `, callbacksCountByEvents);
     }
   });
+};
+
+const compareNumbers = (a: number, b: number) => {
+  if (a < b) return -1;
+  else if (a > b) return 1;
+  else return 0;
 };
